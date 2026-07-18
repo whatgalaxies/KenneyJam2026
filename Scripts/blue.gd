2b7398904ebb014134260abc7cdae9ea9226882c
whatgalaxies <whatgalaxies@gmail.com>
2026-07-18 13:09:15 -0700

    graphicsupdate
    changed the graphics used in the game.

--- Scripts/blue.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func play_idle_animation():
-	%AnimationPlayer.play("Idle")
-
-func play_walk_animation_right():
-	%AnimationPlayer.play("Move Right")
---
+++ player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+
+var speed = 300.0
+const JUMP_VELOCITY = -650.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	#Sprite Flip On Moving Left
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+		
+	#Animations
+	if direction == 0:
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("walk")
+		
+	if velocity.y < 0:
+		animated_sprite_2d.play("jump")
+	
+	if direction:
+		velocity.x = direction * speed
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
